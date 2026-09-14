7bb0ff1f689bc8133b36e77e8c1944df93ec982c
Simba <simbazhou123@gmail.com>
2026-09-14 15:48:22 -0400

    Yay 2

---
+++ src/lab1/Main.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class Main {
+    static void main() {
+        FizzBuzz fb = new FizzBuzz();
+        for (int i = 1; i <= 100; i++) {
+            fb.doFizzBuzz(i);
+        }
+
+    }
+}
